bae1bee88fc93a565abd64378042401ad7aa2d8e
xinru.leng <xinru.leng@ttpod.com>
2013-08-08 14:16:04 +0800

    优先级队列

--- src/kevin/stack/PriorityQueue.java
+++ src/kevin/stack/PriorityQueue.java
@@ -1,11 +1,78 @@
 package kevin.stack;
 
+import java.util.Comparator;
+
 /**
- * Created with IntelliJ IDEA.
- * User: j
- * Date: 13-8-6
- * Time: 上午11:12
- * To change this template use File | Settings | File Templates.
+ * 值越大, 优先级越高
  */
-public class PriorityQueue {
+public class PriorityQueue<E> implements Queue<E>{
+
+
+    private Object[] valaues;
+    private int size;
+    int maxSize;
+
+    private Comparator<E> comparator = new Comparator<E>() {
+        @Override
+        public int compare(E o1, E o2) {
+
+            return o1.hashCode() - o2.hashCode();
+        }
+    };
+
+    public PriorityQueue(int capability) {
+        valaues = new Object[capability];
+        maxSize = capability;
+    }
+
+    @Override
+    public boolean enqueue(E e) {
+        if (isFull()) {
+//            System.out.println("queue is full, enqueue " + e + " failed");
+            return false;
+        }
+
+
+        if(size == 0){
+            valaues[0] = e;
+        } else {
+            int i=0;
+            for(i=size-1;i>=0;i--){
+                E ae = (E)valaues[i];
+                if(comparator.compare(ae, e) > 0){
+                    valaues[i+1] = valaues[i];
+                } else {
+                    break;
+                }
+
+            }
+
+            valaues[i+1] = e;
+        }
+
+
+
+
+        size++;
+        return true;
+    }
+
+    @Override
+    public E dequeue() {
+        if (empty()) {
+            System.out.println("queue is empty, dequeue failed, return null");
+            return null;
+        }
+
+        return (E) valaues[--size];
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == maxSize;
+    }
+
 }
--- src/kevin/stack/QueueApp.java
+++ src/kevin/stack/QueueApp.java
@@ -9,38 +9,39 @@ package kevin.stack;
  */
 public class QueueApp {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayQueue<Integer>(5);
+//        Queue<Integer> queue = new ArrayQueue<Integer>(5);
+        Queue<Integer> queue = new PriorityQueue<Integer>(5);
 
         Integer e;
         e = queue.dequeue();
-        System.out.println("dequeue " + e);
+        System.out.println("出队 " + e);
 
         for (int i = 0; i < 4; i++) {
             boolean ret = queue.enqueue(i);
-            System.out.println("enqueue " + i + " result: " + ret);
+            System.out.println("入队 " + i + " result: " + ret);
         }
 
         boolean ret = false;
-        ret = queue.enqueue(4);
-        System.out.println("enqueue 4 result: " + ret);
+        ret = queue.enqueue(44);
+        System.out.println("入队 44 result: " + ret);
         ret = queue.enqueue(5);
-        System.out.println("enqueue 5 result: " + ret);
-        ret = queue.enqueue(6);
-        System.out.println("enqueue 6 result: " + ret);
+        System.out.println("入队 5 result: " + ret);
+        ret = queue.enqueue(61);
+        System.out.println("入队 61 result: " + ret);
 
         e = queue.dequeue();
-        System.out.println("dequeue " + e);
+        System.out.println("出队 " + e);
         e = queue.dequeue();
-        System.out.println("dequeue " + e);
+        System.out.println("出队 " + e);
         e = queue.dequeue();
-        System.out.println("dequeue " + e);
+        System.out.println("出队 " + e);
 
         ret = queue.enqueue(6);
-        System.out.println("enqueue 6 result: " + ret);
+        System.out.println("入队 6 result: " + ret);
 
 
         while((e=queue.dequeue()) !=null){
-            System.out.println("dequeue " + e);
+            System.out.println("出队 " + e);
         }
 
     }
